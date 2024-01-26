11e63aa247b53c939a6182b078d6b1d3404baf8c
Voloshko Anton <voloshko.03@gmail.com>
2024-01-26 19:18:54 +0200

    realize rectengel input

--- Program.cs
+++ Program.cs
@@ -4,4 +4,15 @@ var cleaner = new RepetitiveCleaner("beta");
 cleaner.SolidOperation();
 cleaner.SoftOperation();
 
+int width = 5;
+int hight = 3;
+for (int i = 0; i < hight; i++)
+{
+    for(int i=0;i< width; i++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine(); 
+}
+
 
